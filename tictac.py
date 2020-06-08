0fa4155fab3889357ff7d2cf3792128d2b7858b1
borisnimcevic <borisnimcevic@gmail.com>
2020-06-08 18:57:25 +0200

    Adding user input ot the tictactoe game

--- tictac.py
+++ tictac.py
@@ -9,4 +9,15 @@ def printBoard(board):
     print('-+-+-')
     print(board['low-l'] + '|' + board['low-m'] + '|' + board['low-l'])
 
+turn = 'X'
+for i in range(9):
+    printBoard(theBoard)
+    print('Turn for ' + turn +'. Move on which space?')
+    move = input()
+    theBoard[move] = turn
+    if turn == 'X':
+        turn = 'O'
+    else:
+        turn = 'X'
+
 printBoard(theBoard)
